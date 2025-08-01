e313171b9f59c91080c5aaa4ce3d31511318ee72
VALOYY <imyoalv@gmail.com>
2025-08-01 16:41:05 +0700

    Create .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,27 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-SLX59YpTXy1YKOobvL4G5wCdkQVrlmA",
-  authDomain: "djikoe-af532.firebaseapp.com",
-  projectId: "djikoe-af532",
-  storageBucket: "djikoe-af532.firebasestorage.app",
-  messagingSenderId: "604294951466",
-  appId: "1:604294951466:web:3900fead3383d4712d0953",
-  measurementId: "G-8KZ92W74B6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
+export {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+};
